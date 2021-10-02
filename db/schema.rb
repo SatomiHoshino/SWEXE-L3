ef4f3f901b5def9868dd47ac659691e88bbc7c8d
Satomi Hoshino <1911140057p@ed.fuk.kindai.ac.jp>
2021-10-02 13:30:54 +0000

    newのテキスト入力欄の変更

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_13_070402) do
+ActiveRecord::Schema.define(version: 2021_10_02_130947) do
 
   create_table "tweets", force: :cascade do |t|
     t.string "message"
